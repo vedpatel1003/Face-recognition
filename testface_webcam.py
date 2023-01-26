28a1bec387d997ed7099ba4be0c5b59617139b4f
Ved Patel <ved@Veds-MacBook-Air.local>
2023-01-26 17:05:59 +0530

    code to use webcam data

--- testface_webcam.py
+++ testface_webcam.py
@@ -1,2 +1,21 @@
-if __name__ ==  "__main__":
-    print("Hello world")
+import cv2
+
+
+def run_webcam():
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = cap.read()
+        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
+
+        cv2.imshow('frame', rgb)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            # out = cv2.imwrite('capture.jpg', frame)
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    run_webcam()
