0b22417ff1e4da17adf42f4f55872af01e629296
vedpatel1003 <vedpatel987612@gmail.com>
2023-01-29 00:35:52 +0530

    Refectoring

--- run_face_recognition.py
+++ run_face_recognition.py
@@ -1,29 +1,6 @@
-import cv2
+from cascade import run_cascade_classifier
 
-face_cascade = cv2.CascadeClassifier('trained_models/haarcascade_frontalface_default.xml')
-
-
-def run_webcam():
-
-    cap = cv2.VideoCapture(0)
-
-    while True:
-        ret, img = cap.read()
-        if not ret:
-            print("No frame received")
-            continue
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-
-    # rectangle
-        for (x, y, w, h) in faces:
-            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        cv2.imshow('img', img)
-        if cv2.waitKey(1) & 0xff == ord('q'):
-            break
-    cap.release()
-    cap.destroyAllwindows()
+if __name__ == "__main__":
+    run_cascade_classifier()
 
 
-if  __name__ == "__main__":
-    run_webcam()
