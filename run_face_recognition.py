8caabb6feb7c8bb04d9d57d36e75d6c026f5ccce
vedpatel1003 <vedpatel987612@gmail.com>
2023-01-28 22:31:48 +0530

    added rectangle around face

--- run_face_recognition.py
+++ run_face_recognition.py
@@ -1,21 +1,18 @@
 import cv2
 
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-def run_webcam():
-    cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture()
+while True:
+    _, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
 
-    while True:
-        ret, frame = cap.read()
-        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2BGRA)
-
-        cv2.imshow('frame', rgb)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            # out = cv2.imwrite('capture.jpg', frame)
-            break
-
-    cap.release()
-    cv2.destroyAllWindows()
-
-
-if __name__ == "__main__":
-    run_webcam()
+# rectangle
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    cv2.imshow('img', img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+cap.release()
